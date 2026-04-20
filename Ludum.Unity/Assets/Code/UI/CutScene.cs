545f09cf86cfa787b2da04de72dc4a6cffb7e7f7
cmd <purelifestudio.mastering@gmail.com>
2026-04-21 02:48:41 +0300

    Snapshot off

--- Ludum.Unity/Assets/Code/UI/CutScene.cs
+++ Ludum.Unity/Assets/Code/UI/CutScene.cs
@@ -2,18 +2,36 @@
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using FMODUnity;
 
 namespace Code.UI
 {
     public class CutScene : MonoBehaviour
     {
+        [Header("Cutscene Settings")]
         [SerializeField] private float _animationDuration;
+
+        [Header("FMOD Events")]
+        [SerializeField] private EventReference event1;
+        [SerializeField] private EventReference event2;
+        [SerializeField] private EventReference event3;
+        [SerializeField] private EventReference event4;
+        [SerializeField] private EventReference event5;
+        [SerializeField] private EventReference event6;
+        [SerializeField] private EventReference event7;
 
         private async void Start()
         {
             await UniTask.Delay(TimeSpan.FromSeconds(_animationDuration));
-
             SceneManager.LoadScene(sceneBuildIndex: 1);
         }
+
+        public void PlayEvent1() => RuntimeManager.PlayOneShot(event1, transform.position);
+        public void PlayEvent2() => RuntimeManager.PlayOneShot(event2, transform.position);
+        public void PlayEvent3() => RuntimeManager.PlayOneShot(event3, transform.position);
+        public void PlayEvent4() => RuntimeManager.PlayOneShot(event4, transform.position);
+        public void PlayEvent5() => RuntimeManager.PlayOneShot(event5, transform.position);
+        public void PlayEvent6() => RuntimeManager.PlayOneShot(event6, transform.position);
+        public void PlayEvent7() => RuntimeManager.PlayOneShot(event7, transform.position);
     }
 }
