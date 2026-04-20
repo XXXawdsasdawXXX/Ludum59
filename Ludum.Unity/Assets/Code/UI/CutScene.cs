558e9aa166c9e7663119f911a493dceb44225861
=) <xxx.awdsasdaw.xxx@gmail.com>
2026-04-21 01:31:49 +0300

    upd cut scene

---
+++ Ludum.Unity/Assets/Code/UI/CutScene.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Code.UI
+{
+    public class CutScene : MonoBehaviour
+    {
+        [SerializeField] private float _animationDuration;
+
+        private async void Start()
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(_animationDuration));
+
+            SceneManager.LoadScene(sceneBuildIndex: 1);
+        }
+    }
+}
